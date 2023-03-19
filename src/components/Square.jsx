b4e3edb1f22a36c53bc20b04735f8e96d183edda
Keeeyaan <keanjieden@gmail.com>
2023-03-19 18:57:55 +0800

    added onClick event listener

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Square = ({ isBlack, children }) => {
+const Square = ({ isBlack, children, onClick }) => {
   return (
     <div
       className={`${
         isBlack ? "bg-[#b58863]" : " bg-[#f0d9b5]"
       } relative w-full h-0 pb-[100%]`}
+      onClick={onClick}
     >
       {children}
     </div>
