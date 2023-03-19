d57a38796db4b85d47af40f5dfd8b31598a800f0
Keeeyaan <keanjieden@gmail.com>
2023-03-19 18:55:30 +0800

    update piece color and display the value

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const Piece = ({ isBlack }) => {
+const Piece = ({ isBlack, value }) => {
   return (
     <div
       className={`${
-        isBlack ? "bg-black" : "bg-white"
+        isBlack ? " bg-red-500" : "bg-blue-500"
       } rounded-full absolute w-4/5 h-4/5 inset-[10%] cursor-pointer`}
-    ></div>
+    >
+      <span className="text-center text-white flex justify-center items-center">
+        {value}
+      </span>
+    </div>
   );
 };
 
