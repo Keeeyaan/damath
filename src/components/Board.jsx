3cd408798ddadb36872d91611ce2efda04a148a3
Keeeyaan <keanjieden@gmail.com>
2023-03-23 22:36:04 +0800

    added multi-capture & square operator features

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -13,6 +13,9 @@ const Board = ({ rows, cols }) => {
   const [selectedPiece, setSelectedPiece] = useState(null);
   const [gameOver, setGameOver] = useState(false);
   const [winner, setWinner] = useState(null);
+  //This will set who's player gonna be first
+  const [currentPlayerIsBlack, setCurrentPlayerIsBlack] = useState(false);
+  const [hasCaptured, setHasCaptured] = useState(false);
 
   const initialPieces = () => {
     const newPieces = pieces.map((row, rowIndex) =>
@@ -45,20 +48,16 @@ const Board = ({ rows, cols }) => {
 
     if (destPiece) return false;
 
-    console.log(srcPiece);
-    console.log(destPiece);
-
     if (rowDiff === 1 && colDiff === 1) {
-      // //This will prevent piece to move backward
-      if (srcPiece.isBlack && destRow < srcRow) {
-        console.log("invalid moveb");
-        return false;
-      } else if (!srcPiece.isBlack && destRow > srcRow) {
-        console.log("invalid movea");
+      //This will prevent piece to move backward
+      if (
+        (srcPiece.isBlack === false && destRow < srcRow) ||
+        (srcPiece.isBlack && destRow > srcRow)
+      ) {
+        return true;
+      } else {
         return false;
       }
-
-      return true;
     } else if (rowDiff === 2 && colDiff === 2) {
       const { row: midRow, col: midCol } = getCapturedPiece(
         srcRow,
@@ -67,7 +66,7 @@ const Board = ({ rows, cols }) => {
         destCol
       );
       const capturedPiece = pieces[midRow][midCol];
-
+      console.log(capturedPiece);
       return capturedPiece && capturedPiece.isBlack !== srcPiece.isBlack;
     }
 
@@ -110,14 +109,41 @@ const Board = ({ rows, cols }) => {
     return true;
   };
 
-  const handleClick = (row, col) => {
+  const hasValidCapture = (row, col) => {
+    for (let newRow = row - 2; newRow <= row + 2; newRow += 4) {
+      for (let newCol = col - 2; newCol <= col + 2; newCol += 4) {
+        if (
+          newRow >= 0 &&
+          newRow < rows &&
+          newCol >= 0 &&
+          newCol < cols &&
+          isValidMove(row, col, newRow, newCol)
+        ) {
+          return true;
+        }
+      }
+    }
+    return false;
+  };
+
+  const handleClick = (row, col, operator) => {
     if (gameOver) return;
-    if (pieces[row][col] && !selectedPiece) {
+
+    console.log(operator);
+
+    const clickedPiece = pieces[row][col];
+
+    if (clickedPiece && clickedPiece.isBlack === currentPlayerIsBlack) {
+      console.log(clickedPiece);
+      console.log(row, col);
       setSelectedPiece({ row, col });
     } else if (
       selectedPiece &&
       isValidMove(selectedPiece.row, selectedPiece.col, row, col)
     ) {
+      const srcPiece = pieces[selectedPiece.row][selectedPiece.col];
+      if (srcPiece.isBlack !== currentPlayerIsBlack) return;
+
       //Create a shallow copy of the pieces to ensure immutability
       const newPieces = pieces.map((row) => [...row]);
 
@@ -138,11 +164,19 @@ const Board = ({ rows, cols }) => {
         );
         newPieces[midRow][midCol] = null;
       }
-
       setPieces(newPieces);
       setSelectedPiece(null);
 
-      checkGameOver(newPieces[row][col].isBlack);
+      const justCaptured = rowDiff === 2 && colDiff === 2;
+      if (justCaptured && hasValidCapture(row, col)) {
+        setSelectedPiece({ row, col });
+        setHasCaptured(true);
+      } else {
+        setHasCaptured(false);
+        if (!checkGameOver(currentPlayerIsBlack)) {
+          setCurrentPlayerIsBlack(!currentPlayerIsBlack);
+        }
+      }
     } else {
       setSelectedPiece(null);
     }
@@ -150,17 +184,60 @@ const Board = ({ rows, cols }) => {
 
   const renderSquares = () => {
     const squares = [];
+    const operators = [
+      "x",
+      "÷",
+      "-",
+      "+",
+      "÷",
+      "x",
+      "+",
+      "-",
+      "-",
+      "+",
+      "x",
+      "÷",
+      "+",
+      "-",
+      "÷",
+      "x",
+      "x",
+      "÷",
+      "-",
+      "+",
+      "÷",
+      "x",
+      "+",
+      "-",
+      "-",
+      "+",
+      "x",
+      "÷",
+      "+",
+      "-",
+      "÷",
+      "x",
+    ];
+    let operatorIndex = 0;
+
     for (let row = 0; row < rows; row++) {
       for (let col = 0; col < cols; col++) {
         const isBlack = (row + col) % 2 === 1;
         const key = `${row}-${col}`;
         const piece = pieces[row][col];
+        const operator = (row + col) % 2 === 0 && operators[operatorIndex++];
         squares.push(
           <Square
             key={key}
             isBlack={isBlack}
-            onClick={() => handleClick(row, col)}
+            onClick={() => handleClick(row, col, operator)}
+            operator={operator}
           >
+            <div className="h-full w-full flex justify-center items-center">
+              <p className=" text-slate-700 font-bold text-xl cursor-default">
+                {operator}
+              </p>
+            </div>
             {piece && (
               <Piece
                 isBlack={piece.isBlack}
@@ -181,22 +258,24 @@ const Board = ({ rows, cols }) => {
 
   return (
     <>
-      <div
-        className="grid gap-[1px] w-full max-w-[500px] h-full max-h-[500px] my-0 mx-auto]"
-        style={{
-          gridTemplateRows: `repeat(${rows}, 1fr)`,
-          gridTemplateColumns: `repeat(${cols}, 1fr)`,
-        }}
-      >
-        {renderSquares()}
-      </div>
-
-      {gameOver && (
-        <div className="text-center mt-4">
-          <h2>Game Over!</h2>
-          <h3>{winner} player wins!</h3>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mx-auto">
+        <div
+          className="grid gap-[1px] w-full max-w-[500px] h-full max-h-[500px]"
+          style={{
+            gridTemplateRows: `repeat(${rows}, 1fr)`,
+            gridTemplateColumns: `repeat(${cols}, 1fr)`,
+          }}
+        >
+          {renderSquares()}
         </div>
-      )}
+
+        {gameOver && (
+          <div className="text-center mt-4">
+            <h2>Game Over!</h2>
+            <h3>{winner} player wins!</h3>
+          </div>
+        )}
+      </div>
     </>
   );
 };
