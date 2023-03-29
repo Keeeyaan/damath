f0e1bbb25a924f9a5c03bca6d16c19f1a07036d5
Keeeyaan <keanjieden@gmail.com>
2023-03-30 01:00:29 +0800

    added current turn color

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Timer from "../hooks/Timer";
+import Timer from "./Timer";
 
 const ScoreBoard = ({
   playerBlueScore,
@@ -9,39 +9,57 @@ const ScoreBoard = ({
   setRedTimeLeft,
   blueTimeLeft,
   setBlueTimeLeft,
+  currentTurn,
 }) => {
   return (
     <div className="h-[450px] w-[500px] bg-[#e6bb99] shadow-md rounded p-6 ">
-      <div className="flex justify-around">
-        <div>
-          {/* <Timer
-            timeLeft={blueTimeLeft}
-            setTimeLeft={setBlueTimeLeft}
-            runOnStart
-          /> */}
-          <div className="bg-[#f0d9b5] p-4 rounded text-center">
-            <h1 className=" font-bold text-blue-500 mb-2 text-xl ">
-              Player Blue
-            </h1>
-            <span className=" font-bold text-3xl text-gray-900">
-              {playerBlueScore}
-            </span>
+      <div className="flex flex-col justify-evenly gap-20">
+        <div className="flex justify-around">
+          <div>
+            <Timer
+              className="font-semibold text-center text-3xl mb-2"
+              timeLeft={blueTimeLeft}
+              setTimeLeft={setBlueTimeLeft}
+              runOnStart
+            />
+            <div className="bg-[#f0d9b5] p-4 rounded text-center">
+              <h1 className=" font-bold text-blue-500 mb-2 text-xl ">
+                Player Blue
+              </h1>
+              <span className=" font-bold text-3xl text-gray-900">
+                {playerBlueScore}
+              </span>
+            </div>
+          </div>
+          <div>
+            <Timer
+              className="font-semibold text-center text-3xl mb-2"
+              timeLeft={redTimeLeft}
+              setTimeLeft={setRedTimeLeft}
+              runOnStart
+            />
+            <div className="bg-[#f0d9b5] p-4 rounded text-center">
+              <h1 className=" font-bold text-red-500 mb-2 text-xl ">
+                Player Red
+              </h1>
+              <span className=" font-bold text-3xl text-gray-900">
+                {playerRedScore}
+              </span>
+            </div>
           </div>
         </div>
-        <div>
-          {/* <Timer
-            timeLeft={redTimeLeft}
-            setTimeLeft={setRedTimeLeft}
-            runOnStart
-          /> */}
-          <div className="bg-[#f0d9b5] p-4 rounded text-center">
-            <h1 className=" font-bold text-red-500 mb-2 text-xl ">
-              Player Red
+        <div className="text-center text-gray-900 text-3xl font-semibold">
+          {currentTurn ? (
+            <h1 className="">
+              Current Turn Is {""}
+              <span className="text-blue-600 font-bold">Blue</span>
             </h1>
-            <span className=" font-bold text-3xl text-gray-900">
-              {playerRedScore}
-            </span>
-          </div>
+          ) : (
+            <h1 className="">
+              Current Turn Is {""}
+              <span className="text-red-600 font-bold">Red</span>
+            </h1>
+          )}
         </div>
       </div>
     </div>
