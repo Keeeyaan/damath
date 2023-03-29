8ab8e1a83f3033782d7c9837619928a2d66f06ea
Keeeyaan <keanjieden@gmail.com>
2023-03-30 01:02:26 +0800

    update capture modal ui

--- src/components/UI/CaptureModal.jsx
+++ src/components/UI/CaptureModal.jsx
@@ -1,9 +1,15 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 import Modal from "./Modal";
-import Countdown from "react-countdown";
+import Timer from "../Timer";
 
-const CaptureModal = ({ onSubmit, onComplete, captureValue }) => {
+const CaptureModal = ({
+  onSubmit,
+  onComplete,
+  captureValue,
+  currentPlayer,
+}) => {
+  const [timeLeft, setTimeLeft] = useState(5);
   const answerRef = useRef();
 
   const submitHandler = () => {
@@ -13,16 +19,34 @@ const CaptureModal = ({ onSubmit, onComplete, captureValue }) => {
   return (
     <Modal>
       <div className="mb-4 p-5 bg-white rounded">
-        <Countdown date={Date.now() + 10000} onComplete={onComplete} />
-        <div>
+        {!currentPlayer ? (
+          <h1 className="font-medium text-blue-400 text-center text-2xl mb-2">
+            Player Blue
+          </h1>
+        ) : (
+          <h1 className="font-medium text-red-400 text-center text-2xl mb-2">
+            Player Red
+          </h1>
+        )}
+        <hr />
+        <Timer
+          className="my-4 font-semibold text-gray-600 text-center text-3xl"
+          timeLeft={timeLeft}
+          setTimeLeft={setTimeLeft}
+          runOnStart
+          onComplete={onComplete}
+        />
+        <hr />
+        <div className="mt-4">
           <label
-            className="block text-gray-700 text-3xl font-bold mb-2 text-center"
+            className="block text-gray-700 text-4xl font-bold mb-2 text-center"
             htmlFor="useranswer"
           >
             {`${captureValue.capturer} ${captureValue.operator} ${captureValue.captured}`}
           </label>
           <input
             ref={answerRef}
+            autoFocus
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="useranswer"
             type="text"
@@ -32,7 +56,7 @@ const CaptureModal = ({ onSubmit, onComplete, captureValue }) => {
         </div>
         <div className=" flex justify-center">
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition ease-in-out mt-5 text-center"
+            className="bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded transition ease-in-out mt-5 text-center"
             onClick={submitHandler}
           >
             Submit
