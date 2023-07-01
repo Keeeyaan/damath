c26fc49b4c28456edf8d053fb8c9393e085d6f7c
Keeeyaan <keanjieden@gmail.com>
2023-07-01 21:26:18 +0800

    update how to play mechanics

--- src/components/UI/HowToModal.jsx
+++ src/components/UI/HowToModal.jsx
@@ -12,22 +12,24 @@ const HowToModal = ({ isOpen, toggleModal }) => {
                 - Each player has a 10-minute playtime to play out their game
                 <br />
                 <br />
-                - After capture, check if the unsay value nga na capture, for
-                instance a number 6 piece captured a number 9 piece on an
-                addition square, then dapat naay mo pop up na equation for them
-                to solve like 6+9=?
+                - When a player captures a piece, an equation pops up for them
+                to solve based on the values of the captured piece, the piece
+                they moved, and the operator on which the piece landed. For
+                example, if a number 6 piece captures a number 9 piece on an
+                addition square, the equation displayed is 6 + 9 = ?
                 <br />
                 <br />
-                - Give them a 30-second/1-minute interval to solve
+                - The player has 30-second to solve
                 <br />
                 <br />
-                - Then check if the answer is correct. If the answer is correct,
-                they get a point. However, if they do not get the answer
-                correct, they get a penalty of their time, a 30-seconds (or
-                depends) minus to their overall playing time
+                - If the player correctly solves the equation, they receive a
+                point. However, if they provide an incorrect answer, a penalty
+                is imposed by deducting 30 seconds (or a duration that depends)
+                from their overall playing time.
                 <br />
-                <br />- Scoring system: highest points to win until either;
-                mahurot ang playtime or no pieces left on the board
+                <br />- Scoring system: The player with the highest points wins
+                until either their playtime is over or there are no pieces left
+                on the board.
               </p>
 
               <button
