3b7c5b4a72293eba58712aa0253d67cd4f75bd84
keeeyaan <keanjieden@gmail.com>
2024-05-13 11:33:47 +0800

    update game mechanics

--- src/components/UI/HowToModal.jsx
+++ src/components/UI/HowToModal.jsx
@@ -9,27 +9,38 @@ const HowToModal = ({ isOpen, toggleModal }) => {
             <div className=" px-6 py-4">
               <h2 className="text-2xl font-bold mb-4">Game Mechanics:</h2>
               <p className="mb-4">
-                - Each player has a 10-minute playtime to play out their game
+                <span className="font-semibold">1. Playtime Limit:</span> Each
+                player has a maximum of 10 minutes to complete their game.
                 <br />
                 <br />
-                - When a player captures a piece, an equation pops up for them
-                to solve based on the values of the captured piece, the piece
-                they moved, and the operator on which the piece landed. For
-                example, if a number 6 piece captures a number 9 piece on an
-                addition square, the equation displayed is 6 + 9 = ?
+                <span className="font-semibold">2. Equation Prompt:</span> When
+                a player captures a piece, an equation will appear based on the
+                values of the captured piece, the moving piece, and the operator
+                square on which the piece lands. For example, if a piece with a
+                value of 6 captures a piece with a value of 9 on an addition
+                square, the equation displayed will be "6 + 9 = ?".
                 <br />
                 <br />
-                - The player has 30-second to solve
+                <span className="font-semibold">3. Time Limit to Solve: </span>
+                Players have 30 seconds to solve each equation.
                 <br />
                 <br />
-                - If the player correctly solves the equation, they receive a
-                point. However, if they provide an incorrect answer, a penalty
-                is imposed by deducting 30 seconds (or a duration that depends)
-                from their overall playing time.
+                <span className="font-semibold">4. Scoring System:</span>
                 <br />
-                <br />- Scoring system: The player with the highest points wins
-                until either their playtime is over or there are no pieces left
-                on the board.
+                <br />
+                <span className="font-semibold">- Correct Answer:</span> If the
+                player correctly solves the equation, they earn one point.
+                <br />
+                <span className="font-semibold">- Incorrect Answer:</span>{" "}
+                Providing an incorrect answer results in a penalty. In addition
+                to the loss of the point, 30 seconds will be deducted from the
+                player's remaining playtime.
+                <br />
+                <br />
+                <span className="font-semibold">5. Winning Condition:</span> The
+                player with the highest score wins. If either player's playtime
+                expires or there are no more pieces left on the board, the game
+                ends.
               </p>
 
               <button
